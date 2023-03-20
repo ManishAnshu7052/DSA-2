4e4e2a2dde48273c843c9cc948f427862ff865d8
Manish <patelmanish5272@gmail.com>
2023-03-20 07:38:46 -0700

    Positive or Negative number

---
+++ src/ControlStatement/Basic/PositiveNegative.java
@@ -0,0 +1,17 @@
+package ControlStatement.Basic;
+import java.util.Scanner;
+public class PositiveNegative {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number =");
+        int num = sc.nextInt();
+        if(num>0){
+            System.out.println("Number is positive");
+        } else if (num<0) {
+            System.out.println("Number is negative");
+        }
+        else {
+            System.out.println("Number is zero");
+        }
+    }
+}
