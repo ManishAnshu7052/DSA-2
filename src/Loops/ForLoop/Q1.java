48dd5ff2d4b76cb006a8a89961111ea21d6d3e5c
Manish <patelmanish5272@gmail.com>
2023-03-24 07:14:57 -0700

    for loop

---
+++ src/Loops/ForLoop/Q1.java
@@ -0,0 +1,11 @@
+package Loops.ForLoop;
+// print 1 to 10 Numbers
+
+public class Q1 {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++){
+            System.out.print(i + " ");
+        }
+    }
+
+}
---
+++ src/Loops/ForLoop/Q2.java
@@ -0,0 +1,9 @@
+package Loops.ForLoop;
+// print your name 5 times
+public class Q2 {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++){
+            System.out.println("Manish Kumar Patel");
+        }
+    }
+}
