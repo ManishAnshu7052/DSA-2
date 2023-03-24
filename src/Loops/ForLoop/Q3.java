e3d43be7e5b684aedb82e69f06bb2438bc6a6a33
Manish <patelmanish5272@gmail.com>
2023-03-24 07:29:30 -0700

    for loop

---
+++ src/Loops/ForLoop/Q3.java
@@ -0,0 +1,11 @@
+package Loops.ForLoop;
+// add 1 to 20 numbers
+public class Q3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i =1; i<=20; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
